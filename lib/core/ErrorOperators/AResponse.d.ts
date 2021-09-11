5dbe2ffd8b3693d85753cf68658371405318e7c6
RicardormDev <romr020606@gmail.com>
2021-09-11 13:23:29 -0500

    feat: AResponse now has error codes & fixed bson lib

--- lib/core/ErrorOperators/AResponse.d.ts
+++ lib/core/ErrorOperators/AResponse.d.ts
@@ -3,29 +3,38 @@ import { OperationResult } from './OperationResult';
  * Represents a generic error, nearly to what a normal error is but in a expected way.
  */
 export declare class AResponse<T, E = Error> extends OperationResult<T, E> {
+    private readonly code;
     private constructor();
+    /**
+     * Returns the designed error code for this error handler.
+     */
+    getErrorCode(): number;
     /**
      * Build a valid response with a value.
      * @param value The response
+     * @param code HTTP Response code
      */
-    static accept<U>(value: U): AResponse<U>;
+    static accept<U>(value: U, code?: number): AResponse<U>;
     /**
      * Build a valid response without content.
      */
-    static acceptEmpty(): AResponse<void>;
+    static acceptEmpty(code?: number): AResponse<void>;
     /**
      * Fail a response with an {@link Error}
      * @param error
+     * @param code
      */
-    static fail<U>(error: Error): AResponse<U>;
+    static fail<U>(error: Error, code?: number): AResponse<U>;
     /**
      * Build a response with an error as any type.
      * @param error The error obj.
+     * @param code HTTP Response code
      */
-    static failAny<U>(error: any): AResponse<U>;
+    static failAny<U>(error: any, code?: number): AResponse<U>;
     /**
      * Build an failed response with a message encapsulated in an {@link Error}
      * @param error
+     * @param code HTTP Response Code
      */
-    static failWithMessage<U>(error: string): AResponse<U>;
+    static failWithMessage<U>(error: string, code?: number): AResponse<U>;
 }
--- src/core/ErrorOperators/AResponse.ts
+++ src/core/ErrorOperators/AResponse.ts
@@ -6,47 +6,61 @@ import { OperationResult } from './OperationResult';
  */
 export class AResponse<T, E = Error> extends OperationResult<T, E> {
 
-    private constructor(value: T | null, error: E | null) {
+    private readonly code: number;
+
+    private constructor(value: T | null, error: E | null, code: number = 500) {
         super(error, value);
+        this.code = code;
+    }
+
+    /**
+     * Returns the designed error code for this error handler.
+     */
+    public getErrorCode() : number {
+        return this.code;
     }
 
     /**
      * Build a valid response with a value.
      * @param value The response
+     * @param code HTTP Response code
      */
-    public static accept<U>(value: U): AResponse<U> {
-        return new AResponse<U>(value, null);
+    public static accept<U>(value: U, code?: number): AResponse<U> {
+        return new AResponse<U>(value, null, code);
     }
 
     /**
      * Build a valid response without content.
      */
-    public static acceptEmpty(): AResponse<void> {
-        return new AResponse<void>(undefined, null);
+    public static acceptEmpty(code?: number): AResponse<void> {
+        return new AResponse<void>(undefined, null, code);
     }
 
     /**
      * Fail a response with an {@link Error}
      * @param error
+     * @param code
      */
-    public static fail<U>(error: Error): AResponse<U> {
+    public static fail<U>(error: Error, code?: number): AResponse<U> {
         return new AResponse<U>(null, error);
     }
 
     /**
      * Build a response with an error as any type.
      * @param error The error obj.
+     * @param code HTTP Response code
      */
-    public static failAny<U>(error: any): AResponse<U> {
-        return new AResponse<U>(null, error);
+    public static failAny<U>(error: any, code?: number): AResponse<U> {
+        return new AResponse<U>(null, error, code);
     }
 
     /**
      * Build an failed response with a message encapsulated in an {@link Error}
      * @param error
+     * @param code HTTP Response Code
      */
-    public static failWithMessage<U>(error: string): AResponse<U> {
-        return new AResponse<U>(null, new Error(error));
+    public static failWithMessage<U>(error: string, code?: number): AResponse<U> {
+        return new AResponse<U>(null, new Error(error), code);
     }
 
 }
