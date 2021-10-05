20099203438a33ee79dde2cf955c0bc62c48339c
RicardormDev <romr020606@gmail.com>
2021-10-05 16:08:25 -0500

    fix: Optimized DError

--- lib/core/ErrorOperators/DError.d.ts
+++ lib/core/ErrorOperators/DError.d.ts
@@ -5,8 +5,9 @@ export declare class DError extends Error {
      */
     readonly networkError: number;
     readonly internalCode: number;
-    constructor(message?: string, httpCode?: number, internalCode?: number);
+    constructor(message?: string, internalCode?: number, httpCode?: number);
     static buildFromGRPC(error: StatusObject | Partial<StatusObject>): DError;
     static isDError(err: Error): boolean;
     static isErrorCanBeDError(err: StatusObject | Partial<StatusObject>): import("@grpc/grpc-js").MetadataValue[] | undefined;
+    static fromError(error: Error, internalError?: number, networkError?: number): Error;
 }
