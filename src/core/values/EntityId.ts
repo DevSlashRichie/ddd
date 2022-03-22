34d1105446c37ea7059363a3fd284a52a92eaaf8
RicardormDev <romr020606@gmail.com>
2022-03-22 17:49:15 -0600

    feat: Added eq to EntityId

--- src/core/values/EntityId.ts
+++ src/core/values/EntityId.ts
@@ -8,7 +8,7 @@ export class EntityId extends ObjectId {
     /**
      * This constructor is built with a mongoose ObjectID or a string representing the hexadecimal number.
      *
-     * @param id Can be a mongoose ObjectId or a string represting the hexadecimal number. It can also be skipped and
+     * @param id Can be a mongoose ObjectId or a string represting the hexadecimal number. It can also be skipped, and
      * it will return a new ID.
      * @throws when used an invalid hexadecimal representation in string.
      */
@@ -20,5 +20,14 @@ export class EntityId extends ObjectId {
             id = new ObjectId(id);
         super(id ? id : new ObjectId());
     }
-}
 
+    eq(id: EntityId | string): boolean {
+        if(typeof id === "string")
+            id = new EntityId(id);
+
+        if(id === this)
+            return true;
+
+        return this.id.equals(id.id);
+    }
+}
