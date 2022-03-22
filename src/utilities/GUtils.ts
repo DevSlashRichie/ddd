c5aa7c787e0db7d0d35f4b375aabb309c6a34330
RicardormDev <romr020606@gmail.com>
2022-03-22 16:44:50 -0600

    fix: GUtils was not working correctly.

--- src/utilities/GUtils.ts
+++ src/utilities/GUtils.ts
@@ -3,13 +3,13 @@ import { Message as Msg } from 'google-protobuf';
 import { GResponse } from 'ddd-scaffold';
 
 type EndpointResponse<R extends Msg> = (error: ServiceError | null, response: R) => void;
-type UnaryEndpoint<T extends Msg, R extends Msg> = (request: T, callback: EndpointResponse<R>) => ClientUnaryCall;
+type EndpointCall<R extends Msg> = (res: EndpointResponse<R>) => ClientUnaryCall;
 
 export class GUtils {
 
-    public static async completeExtract<T extends Msg, R extends Msg>(request: T, endpoint: UnaryEndpoint<T, R>) {
+    public static async completeExtract<R extends Msg>(endpoint: EndpointCall<R>): Promise<GResponse<{ response: R, call: ClientUnaryCall }>> {
         return await new Promise<GResponse<{ response: R, call: ClientUnaryCall }>>(accept => {
-            const call = endpoint(request, (error, response) => {
+            const call = endpoint((error, response) => {
                 if (error)
                     accept(GResponse.failAny(error));
                 else
@@ -22,8 +22,8 @@ export class GUtils {
     }
 
 
-    public static async extract<T extends Msg, R extends Msg>(request: T, endpoint: UnaryEndpoint<T, R>) : Promise<GResponse<R>> {
-        const value = await this.completeExtract(request, endpoint);
+    public static async extract<R extends Msg>(endpoint: EndpointCall<R>) : Promise<GResponse<R>> {
+        const value = await this.completeExtract(endpoint);
         if(!value.isSuccess())
             return GResponse.failAny(value.getError());
         return GResponse.accept(value.getValue().response);
